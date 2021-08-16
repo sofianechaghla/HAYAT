ed492cc7e3c89b84dc2a4293aaecff4345152f7a
sofianechaghla <77396241+sofianechaghla@users.noreply.github.com>
2021-08-16 18:41:34 +0200

    ajoute newsapi

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use jcobhams\NewsApi\NewsApi;
+class MainController extends Controller
+{
+    public function welcome(){
+        
+
+        $newsapi = new NewsApi("e6368fc1cf994d6e8521d0cd0af0739e");
+        $all_articles=$newsapi->getEverything($q = "réchauffement climatique", $sources = null, $domains = null, $exclude_domains = null, $from = null, $to = null, $language = "fr", $sort_by = null,  $page_size = "5", $page = null);
+        $articles=$all_articles->articles;
+        return view('welcome',compact('articles'));
+            }
+}
